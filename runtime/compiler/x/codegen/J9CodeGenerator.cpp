2842a7c1429a14df4767266eb4fcdfed6aa7a52c
BradleyWood <bradley.wood@ibm.com>
2021-05-09 22:30:36 -0400

    Implement evaluator for inlining of StringLatin1.inflate
    Signed-off-by: BradleyWood <bradley.wood@ibm.com>

--- runtime/compiler/x/codegen/J9CodeGenerator.cpp
+++ runtime/compiler/x/codegen/J9CodeGenerator.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corp. and others
+ * Copyright (c) 2000, 2021 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -75,6 +75,7 @@ J9::X86::CodeGenerator::initialize()
    if (!TR::Compiler->om.canGenerateArraylets())
       {
       cg->setSupportsReferenceArrayCopy();
+      cg->setSupportsInlineStringLatin1Inflate();
       }
 
    if (comp->requiresSpineChecks())
