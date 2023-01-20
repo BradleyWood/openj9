840198821fa152ccc4feb733d791d8267f01ef2e
BradleyWood <bradley.wood@ibm.com>
2023-01-20 14:33:32 -0700

    tmp

--- runtime/compiler/optimizer/VectorAPIExpansion.hpp
+++ runtime/compiler/optimizer/VectorAPIExpansion.hpp
@@ -295,12 +295,12 @@ class TR_VectorAPIExpansion : public TR::Optimization
    static TR::VectorLength supportedOnPlatform(TR::Compilation *comp, vec_sz_t vectorLength)
          {
          // General check for supported infrastructure
-         if (!comp->target().cpu.isPower() &&
-               !(comp->target().cpu.isZ() && comp->cg()->getSupportsVectorRegisters()) &&
-               !comp->target().cpu.isARM64())
-            return TR::NoVectorLength;
+//         if (!comp->target().cpu.isPower() &&
+//               !(comp->target().cpu.isZ() && comp->cg()->getSupportsVectorRegisters()) &&
+//               !comp->target().cpu.isARM64())
+//            return TR::NoVectorLength;
 
-         if (vectorLength != 128)
+         if (vectorLength == 64)
             return TR::NoVectorLength;
 
          TR::VectorLength length = OMR::DataType::bitsToVectorLength(vectorLength);
